667be5cdb2b3cf6f8fed5de7303ecf00e4ab2fa0
mahmoudv2017 <mahmoud.elbermbaly2017@yahoo.com>
2022-09-05 05:58:30 +0200

    first_commit

---
+++ server.js
@@ -0,0 +1,51 @@
+// Setup empty JS object to act as endpoint for all routes
+
+const projectData = {};
+
+// Require Express to run server and routes
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const fetch = require('fetch')
+
+
+// Start up an instance of app
+const app = express()
+
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Cors for cross origin allowance
+app.use(cors())
+
+// Initialize the main project folder
+app.use(express.static('website'));
+
+
+// Setup Server
+app.listen(8000 , () => {
+    console.log(`server started at 8000`)
+})
+
+app.post('/api' , (req,res) => {
+    Object.assign(projectData , req.body)
+    res.status(200).send('done')
+})
+
+app.get("/all" , (_,res) => {
+
+    
+    fetch.fetchUrl(`https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=${projectData.key}`, (error ,meta ,  reponse) => {
+        if(!error && meta.status == 200){
+            let weather_data = JSON.parse(reponse.toString())
+            Object.assign(projectData , weather_data)
+            
+            res.status(200).send(projectData)
+        }
+    })
+   
+    
+})
---
+++ website/app.js
@@ -0,0 +1,56 @@
+/* Global Variables */
+const api_key = "02daa4af18db45d22d12b68e7260b673&units=imperial"
+
+// Create a new date instance dynamically with JS
+let d = new Date();
+let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+const zip_code = document.getElementById('zip').textContent
+
+
+async function  poster_request(){
+  const post_req = await fetch('/api' , {
+    method : 'POST',
+    headers : {
+      "Content-Type" : 'application/json'
+    },
+    body : JSON.stringify( {date : newDate , key : api_key}),
+    
+  })
+
+  return post_req
+}
+
+
+
+
+
+const retrieveData = async () =>{
+
+
+    await poster_request()
+ 
+    await fetch('/all').then(async results => {
+      try {
+        // Transform into JSON
+      
+        const allData = await results.json()
+        console.log(allData)
+        // Write updated data to DOM elements
+        document.getElementById('temp').innerHTML = Math.round(allData.main.temp)+ 'degrees';
+        document.getElementById('content').innerHTML = allData.main.feels_like;
+        document.getElementById("date").innerHTML = allData.date;
+        }
+        catch(error) {
+          console.log("error", error);
+          // appropriately handle the error
+        }
+    })
+   
+   }
+
+//making my events
+
+
+
+document.getElementById('generate').addEventListener('click' , retrieveData)
+
